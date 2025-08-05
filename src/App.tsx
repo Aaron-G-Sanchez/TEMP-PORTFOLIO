c460878670530ef912eb126f0c64f3f4e7f231e8
Aaron Sanchez <saarong12@gmail.com>
2025-08-04 21:44:08 -0500

    Added initial portfolio content.

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import './App.scss'
+
+export const App = () => {
+  const handleClick = () => {
+    window.location.href = 'mailto:contact@aarongsanchez.me'
+  }
+
+  return (
+    <>
+      <main>
+        <div className="'subheading">
+          <h2 className="name">Aaron G Sanchez</h2>
+          <p className="job-title">
+            Software Engineer | Audio Engineer | Burrito Enthusiast
+          </p>
+        </div>
+
+        <div className="heading">
+          <h1 className="title">Hi, I'm Aaron.</h1>
+          <p className="description">
+            This page is currently under construction.
+          </p>
+        </div>
+        <div className="btn-container">
+          <button onClick={handleClick} className="btn">
+            Contact Me
+          </button>
+        </div>
+      </main>
+    </>
+  )
+}
